64de4eb9ace5395ee2a8ee744d1fdadeab4e2505
C-E-T <yocode246@gmail.com>
2020-09-19 16:35:50 -0400

    change html to php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-echo "My first PHP";
+<!--Index.html 
+Written by Cody Tersigni-->
+
+<!Doctype HTML>
+
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="Styles/index_Style.css">
+	<script src="JavaScript/indexJavaScript.js"></script>
+	
+  </head>
+  
+  <body onload = "checkCookie()">	
+    <?php echo"Hello world"?>
+  </body>
+
+</html>
